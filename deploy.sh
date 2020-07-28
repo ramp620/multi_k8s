fe272b45730fbe2e32a9c2ae6475eff6054b9fe0
Your Name <you@example.com>
2020-07-28 02:30:56 -0400

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 docker build - t ramp620/multi-client:latest -t ramp620/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build - t ramp620/multi-server:latest -t ramp620/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build - t ramp620/multi-worker:latest -t ramp620/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push ramp620/multi-client:latest
 docker push ramp620/multi-server:latest
 docker push ramp620/multi-worker:latest
 
-docker push ramp620/multi-client:latest/ramp620/multi-client:$SHA
-docker push ramp620/multi-server:latest/ramp620/multi-server:$SHA
-docker push ramp620/multi-worker:latest/ramp620/multi-worker:$SHA
+docker push ramp620/multi-client:$SHA
+docker push ramp620/multi-server:$SHA
+docker push ramp620/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=ramp620/multi-server:$SHA
-kubectl set image deployments/server-deployment client=ramp620/multi-client:$SHA
-kubectl set image deployments/server-deployment worker=ramp620/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=ramp620/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=ramp620/multi-worker:$SHA
